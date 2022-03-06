5c72c5c31475334f5645ec94423e400080e90696
devnamrits <hrk10321@gmail.com>
2022-03-06 19:39:59 +0530

    wrote two points about functional programming

--- src/FunctionalProgramming/FPNumberRunner.java
+++ src/FunctionalProgramming/FPNumberRunner.java
@@ -2,6 +2,13 @@ package FunctionalProgramming;
 
 import java.util.List;
 
+/*
+ * Two important things in Functional Programming:
+ *   # Avoid mutation, usage of local variables etc.
+ *   # We just need to tell what exactly we have to do. For example we just telling to pick the stream of numbers
+ *     and add it up
+ * */
+
 public class FPNumberRunner {
     public static void main(String[] args) {
         List<Integer> list = List.of(4, 6, 8, 13, 3, 15);
